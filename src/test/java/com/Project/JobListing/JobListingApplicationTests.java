8574f9eecd141cdbd7a129ad77615c8fe7b2222f
Meet <meetshah7199@gmail.co>
2024-01-11 12:26:26 -0500

    Start with Job Listing Project

---
+++ src/test/java/com/Project/JobListing/JobListingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project.JobListing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobListingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
