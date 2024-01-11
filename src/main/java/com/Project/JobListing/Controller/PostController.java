674f7d582e3bda6911a7a87081bde0d3bdfc135e
Meet <meetshah7199@gmail.co>
2024-01-11 12:54:48 -0500

    Create Model and Controller

--- src/main/java/com/Project/JobListing/Controller/PostController.java
+++ src/main/java/com/Project/JobListing/Controller/PostController.java
@@ -2,6 +2,8 @@ package com.Project.JobListing.Controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.annotations.ApiIgnore;
+
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class PostController {
+
+    @ApiIgnore
     @RequestMapping("/")
     public void redirect(HttpServletResponse response) throws IOException {
         response.sendRedirect("/swagger-ui.html");
