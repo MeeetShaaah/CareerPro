0fe0e29a4b508c78f836ebe498397ea3a8f5cf9e
MeeetShaaah <meetshah7199@gmail.com>
2024-02-26 23:39:10 -0500

    Deployment

--- joblisting-frontend/src/services/api.js
+++ joblisting-frontend/src/services/api.js
@@ -2,7 +2,8 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api'; // Update with your backend API base URL
+// const API_BASE_URL = 'http://localhost:8080/api'; // Update with your backend API base URL
+const API_BASE_URL = 'joblisting-production.up.railway.app';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
